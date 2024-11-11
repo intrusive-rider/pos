8d14fce1e62d1384d5c922d5e104c0d1992343a5
intrusive-rider <intrusive-rider@proton.me>
2024-11-11 10:53:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# POS
+
+Hi, this is a POS project...
+What the hell is a POS anyway?
+
+> [!IMPORTANT]  
+> This project is currently on development.
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require("daisyui"), forms],
+};
