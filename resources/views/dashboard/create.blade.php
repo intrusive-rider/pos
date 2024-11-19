ebb8933487a124cb14dcb4e287290afd1016f7fe
intrusive-rider <intrusive-rider@proton.me>
2024-11-19 13:01:30 +0700

    Added QOL things?

--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -1,12 +1,17 @@
 <x-layouts.app class="pb-16">
-    <section class="flex justify-between items-end">
-        <div class="space-y-4 max-w-prose">
-            <h1 class="text-5xl font-bold">New order</h1>
-            <p class="text-4xl">Select their items below.</p>
-        </div>
-        <a href="/" class="btn btn-lg btn-primary">Check out</a>
+    <section class="space-y-4 max-w-prose">
+        <h1 class="text-5xl font-bold">New order</h1>
+        <p class="text-4xl">Select their items below.</p>
     </section>
-    <div class="divider"></div>
+    <section class="flex items-center justify-between sticky top-0 z-10 backdrop-blur-sm">
+        <x-layouts.form method="POST" action="/search">
+            <x-forms.input name="q" icon="magnifying-glass" :placeholder="__('Search product')" />
+        </x-layouts.form>
+        <div>
+            <a href="/" class="btn btn-lg btn-ghost">Cancel</a>
+            <a href="/" class="btn btn-lg btn-primary">Check out</a>
+        </div>
+    </section>
     <section class="space-y-6 pb-12">
         <h2 class="text-3xl font-semibold">Popular items</h2>
         <div class="grid grid-cols-3 gap-6">
