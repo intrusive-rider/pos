1a2a4b51ad6c25b9fe5e0608930589c79eb9a175
intrusive-rider <intrusive-rider@proton.me>
2024-11-19 13:54:56 +0700

    Updated action cards and added Livewire

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,9 +1,15 @@
+@props([
+    'title' => 'Lorem',
+    'price' => '10.000,00'
+])
+
 <div class="card lg:card-side card-compact bg-base-100 shadow-xl">
     <figure>
         <img src="https://placehold.co/400" />
     </figure>
     <div class="card-body justify-between w-56">
-        <h2 class="card-title text-2xl">{{ $slot }}</h2>
+        <h2 class="card-title text-2xl">{{ $title }}</h2>
+        <p>Rp{{ $price }}</p>
         <div class="flex items-center join">
             <button class="btn join-item">
                 <i class="ph-bold ph-minus text-lg"></i>
--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -12,34 +12,34 @@
     <section class="space-y-6 pb-12">
         <h2 class="text-3xl font-semibold">Popular items</h2>
         <div class="grid grid-cols-3 gap-6">
-            <x-product>Chair</x-product>
-            <x-product>Paint</x-product>
-            <x-product>Bed cover</x-product>
-            <x-product>Screwdriver</x-product>
-            <x-product>Plate</x-product>
-            <x-product>Phone holder</x-product>
+            <x-product title="Chair" />
+            <x-product title="Paint" />
+            <x-product title="Bed cover" />
+            <x-product title="Screwdriver" />
+            <x-product title="Plate" />
+            <x-product title="Phone holder" />
         </div>
     </section>
     <section class="space-y-6 pb-12">
         <h2 class="text-3xl font-semibold">Bundles</h2>
         <div class="grid grid-cols-3 gap-6">
-            <x-product>Chair</x-product>
-            <x-product>Paint</x-product>
-            <x-product>Bed cover</x-product>
-            <x-product>Screwdriver</x-product>
-            <x-product>Plate</x-product>
-            <x-product>Phone holder</x-product>
+            <x-product title="Chair" />
+            <x-product title="Paint" />
+            <x-product title="Bed cover" />
+            <x-product title="Screwdriver" />
+            <x-product title="Plate" />
+            <x-product title="Phone holder" />
         </div>
     </section>
     <section class="space-y-6 pb-12">
-        <h2 class="text-3xl font-semibold">Households</h2>
+        <h2 class="text-3xl font-semibold">20&percnt; off</h2>
         <div class="grid grid-cols-3 gap-6">
-            <x-product>Chair</x-product>
-            <x-product>Paint</x-product>
-            <x-product>Bed cover</x-product>
-            <x-product>Screwdriver</x-product>
-            <x-product>Plate</x-product>
-            <x-product>Phone holder</x-product>
+            <x-product title="Chair" />
+            <x-product title="Paint" />
+            <x-product title="Bed cover" />
+            <x-product title="Screwdriver" />
+            <x-product title="Plate" />
+            <x-product title="Phone holder" />
         </div>
     </section>
 </x-layouts.app>
