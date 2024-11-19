5efa28b22af10488457c78f8bc9af779d86925d8
intrusive-rider <intrusive-rider@proton.me>
2024-11-19 15:56:58 +0700

    Implemented Alpine to counter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
-import './count';
 
 window.Alpine = Alpine;
 Alpine.start();
--- resources/views/components/counter.blade.php
+++ resources/views/components/counter.blade.php
@@ -1,11 +1,10 @@
-<div class="flex items-center join h-8" data-counter>
-    <button class="btn btn-sm join-item" data-minus>
+<div x-data="{ count: 0 }" class="flex items-center join h-8">
+    <button x-on:click="count--" class="btn btn-sm join-item" :disabled="count <= 0">
         <i class="ph-bold ph-minus"></i>
     </button>
-    <input type="number" value="0"
-        class="border-none focus:ring-0 h-full join-item w-16 bg-base-200 tabular-nums slashed-zero flex items-center justify-center text-center no-spinner"
-        data-display />
-    <button class="btn btn-sm join-item" data-plus>
+    <input type="number" x-model="count"
+        class="border-none focus:ring-0 h-full join-item w-16 bg-base-200 tabular-nums slashed-zero flex items-center justify-center text-center no-spinner" />
+    <button x-on:click="count++" class="btn btn-sm join-item">
         <i class="ph-bold ph-plus"></i>
     </button>
 </div>
