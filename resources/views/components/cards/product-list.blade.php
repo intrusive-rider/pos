06a0a8d18830db2576062aca377384ace3fa2176
intrusive-rider <intrusive-rider@proton.me>
2024-11-21 16:20:53 +0700

    Adjusted tabular nums

--- resources/views/components/cards/product-list.blade.php
+++ resources/views/components/cards/product-list.blade.php
@@ -14,7 +14,7 @@
     </figure>
     <div class="card-body flex-row items-center justify-between">
         <h2 class="card-title text-3xl">{{ $title }}</h2>
-        <p class="text-lg text-right tabular-nums">
+        <p class="text-lg text-right">
             Rp{{ number_format($price, 2, ',', '.') }} &times; {{ $qty }} <br />
             <span class="text-lg font-semibold tabular-nums">Rp{{ number_format($qty_price, 2, ',', '.') }}</span>
         </p>
