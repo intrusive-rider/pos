3875e72f887d6e3c3f006baa59fd23a9ff5ce659
intrusive-rider <intrusive-rider@proton.me>
2024-11-22 11:24:06 +0700

    Mengupdate class warna pada action card

--- resources/views/pos/home.blade.php
+++ resources/views/pos/home.blade.php
@@ -4,14 +4,14 @@
         <p class="text-2xl">What would you like to do?</p>
     </section>
     <section class="grid grid-cols-3 gap-6">
-        <x-cards.action href="{{ route('new-transaction') }}" icon="shopping-cart" color="green">New order</x-cards.action>
-        <x-cards.action href="/" icon="package">Update stocks</x-cards.action>
-        <x-cards.action href="/" icon="seal-percent" color="pink">20% off</x-cards.action>
-        <x-cards.action href="https://example.com" icon="link" color="amber">
+        <x-cards.action href="{{ route('new-transaction') }}" icon="shopping-cart" class="bg-green-500">New order</x-cards.action>
+        <x-cards.action href="/" icon="package" class="bg-blue-500">Update stocks</x-cards.action>
+        <x-cards.action href="/" icon="seal-percent" class="bg-pink-500">20% off</x-cards.action>
+        <x-cards.action href="https://example.com" icon="link" class="bg-amber-500">
             Visit website
             <p class="font-normal text-xl">example.com</p>
         </x-cards.action>
-        <x-cards.action href="/" icon="truck">Send to customer</x-cards.action>
-        <x-cards.action href="/" icon="scroll">Report sales</x-cards.action>
+        <x-cards.action href="/" icon="truck" class="bg-blue-500">Send to customer</x-cards.action>
+        <x-cards.action href="/" icon="scroll" class="bg-blue-500">Report sales</x-cards.action>
     </section>
 </x-layouts.app>
