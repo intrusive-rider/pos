4b0f27274b4789492318907d7c1cbedd944d72eb
intrusive-rider <intrusive-rider@proton.me>
2024-11-22 14:38:23 +0700

    Merbaikin styling action card

--- resources/views/components/cards/action.blade.php
+++ resources/views/components/cards/action.blade.php
@@ -9,7 +9,7 @@
         'p-8 h-64',
         'text-white text-4xl font-bold',
         'rounded-xl shadow-lg',
-        'border-b-8 border-base-200',
+        'border-b-8 border-black/10',
         'transition-opacity',
     ];
 
