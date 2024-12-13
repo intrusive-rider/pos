bfa9538266ddf3bfbc75047f96fd7ea85a275166
intrusive-rider <intrusive-rider@proton.me>
2024-12-13 12:26:05 +0700

    Menambahkan padding

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <x-layouts.import />
 
-<body class="antialiased flex flex-col bg-base-100 min-h-screen mx-auto max-w-5xl">
+<body class="antialiased flex flex-col bg-base-100 min-h-screen mx-auto max-w-5xl px-10">
     <x-layouts.header />
     @livewireScripts
     <main {{ $attributes(['class' => 'space-y-8 w-full']) }}>
